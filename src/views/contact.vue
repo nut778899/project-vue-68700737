28a45ec767eef8e24098892a964528b99b296bc9
nut778899 <nut663696@gmail.com>
2026-03-11 06:48:48 -0700

    first commit

---
+++ src/views/contact.vue
@@ -0,0 +1,26 @@
+<template lang="">
+    <div class ="container">
+        <div class="alert alert-info" role="alert">
+  A simple info alert—check it out!
+</div>
+
+        <h1>Show Contact</h1>
+        <h3>Nataly Laoly</h3>
+        <p>Wolcome to vue</p>
+</div>
+
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+h3{
+ color: rgb(38, 0 , 255);
+}
+    
+</style>
